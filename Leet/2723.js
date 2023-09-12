d40a822ee0dacb654f77c9a08a7a2936e69558ca
Chubbar5 <connor.hubbard5@gmail.com>
2023-09-12 14:06:06 -0400

    pushing new changes to Main.

---
+++ Leet/2723.js
@@ -0,0 +1,8 @@
+var addTwoPromises = async function(promise1, promise2) {
+    var promiseCombined = promise1.resolve + promise2.resolve
+
+};
+
+var c = a;
+var a = b;
+var b = c;
---
+++ section-14/learning.js
@@ -0,0 +1,7 @@
+function lifeInWeek(age){
+    var yearsRemaning = 90-age;
+    var weeksRemaining = yearsRemaning * 52;
+    var monthsremaining = yearsRemaning * 12;
+}
+
+lifeInWeek(26);
